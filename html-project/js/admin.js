b987cc9ba0d1a05283a5af3a734489fce9659007
shreeshm217-j <shreeshm217@gmail.com>
2026-03-13 19:18:48 +0530

    Update admin.js

--- html-project/js/admin.js
+++ html-project/js/admin.js
@@ -1,5 +1,25 @@
 // Admin Dashboard Functionality
 
+import {
+firebaseAddMenu,
+firebaseUpdateMenu,
+firebaseDeleteMenu
+} from "./firebase-sync.js";
+
+import {
+getMenuItems,
+getCategories,
+getOrders,
+getSettings,
+getGalleryImages,
+addCategory,
+updateCategory,
+deleteCategory,
+addGalleryImage,
+deleteGalleryImage,
+updateSettings
+} from "./data.js";
+
 let currentTab = 'menu';
 let editingItemId = null;
 let editingCategoryId = null;
@@ -453,4 +473,18 @@ function handleDeleteGalleryImage(imageId) {
         showToast('Image deleted successfully');
         loadGalleryImages();
     }
+}
+
+// Firebase menu wrappers (keep old function names working)
+
+function addMenuItem(itemData) {
+    firebaseAddMenu(itemData);
+}
+
+function updateMenuItem(id, data) {
+    firebaseUpdateMenu(id, data);
+}
+
+function deleteMenuItem(id) {
+    firebaseDeleteMenu(id);
 }
